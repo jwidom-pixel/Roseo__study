8c664f9d4951d1c056157bbef05910bc5ba93bd2
jwidom-pixel <phaesol81@gmail.com>
2024-12-23 18:58:13 +0900

    메인 테마 적용

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,7 @@ import 'calendar/calendar_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-      //options: DefaultFirebaseOptions.currentPlatform,
-      );
+  await Firebase.initializeApp();
   runApp(ProviderScope(child: MyApp()));
 }
 
@@ -15,6 +13,42 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      //테마 지정
+      theme: ThemeData(
+        dialogTheme: DialogTheme(
+          backgroundColor: Colors.grey[50],
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+          titleTextStyle: TextStyle(
+            color: Color.fromARGB(255, 65, 65, 65),
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+          contentTextStyle: TextStyle(
+            color: Colors.black87,
+            fontSize: 16,
+          ),
+        ),
+        // 버튼 테마 설정
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            foregroundColor: Color.fromARGB(255, 65, 65, 65), // 버튼 텍스트 색상
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.white, // 텍스트 색상
+            backgroundColor: Color.fromARGB(255, 65, 65, 65), // 버튼 배경색
+          ),
+        ),
+        outlinedButtonTheme: OutlinedButtonThemeData(
+          style: OutlinedButton.styleFrom(
+            foregroundColor: Color.fromARGB(255, 65, 65, 65), // 버튼 텍스트 색상
+          ),
+        ),
+      ),
+      //
       debugShowCheckedModeBanner: false,
       home: CalendarPage(),
     );
